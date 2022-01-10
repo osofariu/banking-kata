8d8c74ae0439e7d882b23d28cbfb40b0ea0d186d
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 18:27:32 -0500

    red -  ledger records a transaction with amount and type

--- src/ledger.spec.ts
+++ src/ledger.spec.ts
@@ -13,4 +13,14 @@ describe('AccountLedger', () => {
 
     expect(transactions.length).toEqual(0)
   })
+
+  it('on recordTransaction ledger has a record of that transaction', () => {
+    ledger.recordDeposit(12)
+
+    expect(ledger.transactions.length).toEqual(1)
+
+    let transaction = ledger.transactions[0]
+    expect(transaction.amount).toEqual(12)
+    expect(transaction.type).toEqual(TransactionType.DEPOSIT)
+  })
 })
