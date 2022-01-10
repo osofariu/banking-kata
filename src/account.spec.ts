282e99714ed3a23104be46ba6a9b269255012947
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 18:17:11 -0500

    red - can deposit a positive amount - delegates to ledger

--- src/account.spec.ts
+++ src/account.spec.ts
@@ -1,14 +1,24 @@
 import {Account} from "./account";
+import createSpyObj = jasmine.createSpyObj;
+import {Ledger} from "./ledger";
 
 describe('Account', () => {
 
   let account: Account
+  let mockLedger: jasmine.SpyObj<Ledger>
 
   beforeEach(() => {
-    account = new Account()
+    mockLedger = createSpyObj(Ledger, ['recordDeposit'])
+    account = new Account(mockLedger)
   })
 
   it('printing statement with no transactions, shows header', () => {
     expect(account.printStatement()).toEqual('Date,Amount,Balance\n')
+  })
+
+  it('can deposit a positive amount', () => {
+    account.deposit(123)
+
+    expect(mockLedger.recordDeposit).toHaveBeenCalledOnceWith(123)
   })
 })
