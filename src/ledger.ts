c146ae3a8a3cb50d543d28ee36e8e343cbaaf1af
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 21:02:10 -0500

    green - recordDeposit returns error on ZERO amount

--- src/ledger.ts
+++ src/ledger.ts
@@ -3,10 +3,13 @@ const {DateTime} = require("luxon");
 export class Ledger {
   private _transaction: Transaction[] = []
 
-  public recordDeposit(amount: number) {
+  public recordDeposit(amount: number): Status {
     if (amount > 0) {
       const dateNow = DateTime.now().toFormat('yyyy-MM-dd')
       this._transaction.push({amount: amount, type: TransactionType.DEPOSIT, date: dateNow})
+      return {success: true}
+    } else {
+      return {success: false, message: "Amount must be positive"}
     }
   }
 
@@ -23,4 +26,9 @@ export type Transaction = {
   amount: number,
   type: TransactionType,
   date: string
+}
+
+export type Status = {
+  success: boolean,
+  message?: string
 }
