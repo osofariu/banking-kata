337a0391883dd07f0e8e166ca31e703a31f79f2d
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 20:56:38 -0500

    green - recordDeposit does not allow ZERO amount

--- src/ledger.ts
+++ src/ledger.ts
@@ -4,8 +4,10 @@ export class Ledger {
   private _transaction: Transaction[] = []
 
   public recordDeposit(amount: number) {
-    const dateNow = DateTime.now().toFormat('yyyy-MM-dd')
-    this._transaction.push({amount: amount, type: TransactionType.DEPOSIT, date: dateNow})
+    if (amount > 0) {
+      const dateNow = DateTime.now().toFormat('yyyy-MM-dd')
+      this._transaction.push({amount: amount, type: TransactionType.DEPOSIT, date: dateNow})
+    }
   }
 
   public get transactions(): Transaction[] {
