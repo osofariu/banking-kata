414f9e36b95296a36f1b88dbc48ca7645b2e45e1
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 20:31:34 -0500

    green - refactor account spec - use helper to mock account with transactions

--- src/account.spec.ts
+++ src/account.spec.ts
@@ -1,29 +1,8 @@
 import {Account} from "./account";
 import createSpyObj = jasmine.createSpyObj;
-import {Ledger, TransactionType} from "./ledger";
+import {Ledger, Transaction, TransactionType} from "./ledger";
 
 describe('Account', () => {
-
-
-  describe('printStatement', () => {
-    it('printing statement with no transactions, shows header', () => {
-      let ledger = new Ledger()
-      spyOnProperty(ledger, 'transactions').and.returnValue([])
-      let account = new Account(ledger)
-
-      expect(account.printStatement()).toEqual('Date,Amount,Balance\n')
-    })
-
-    it('prints statement with one transaction', () => {
-      let ledger = new Ledger()
-      spyOnProperty(ledger, 'transactions').and.returnValue(
-        [{amount: 1234, type: TransactionType.DEPOSIT, date: "2021-05-29"}])
-      let account = new Account(ledger)
-
-      expect(account.printStatement()).toEqual('Date,Amount,Balance\n' +
-        '2021-05-29,+1234,1234\n')
-    })
-  })
 
   describe('deposit', () => {
     let account: Account
@@ -41,4 +20,25 @@ describe('Account', () => {
     })
   })
 
+  describe('printStatement', () => {
+    it('printing statement with no transactions, shows header', () => {
+      let account = accountWithTransactions([])
+
+      expect(account.printStatement()).toEqual('Date,Amount,Balance\n')
+    })
+
+    it('prints statement with one transaction', () => {
+      let account = accountWithTransactions(
+        [{amount: 1234, type: TransactionType.DEPOSIT, date: "2021-05-29"}])
+
+      expect(account.printStatement()).toEqual('Date,Amount,Balance\n' +
+        '2021-05-29,+1234,1234\n')
+    })
+  })
+
+  function accountWithTransactions(transactions: Transaction[]) {
+    let ledger = new Ledger()
+    spyOnProperty(ledger, 'transactions').and.returnValue(transactions)
+    return new Account(ledger)
+  }
 })
