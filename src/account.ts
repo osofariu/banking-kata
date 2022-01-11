9e2bca740dda74ba00ad916118b02c863560cad6
Ovi Sofariu <ovi@sofariu.com>
2022-01-10 20:44:32 -0500

    green account printStatement calculates balance

--- src/account.ts
+++ src/account.ts
@@ -8,8 +8,10 @@ export class Account {
   public printStatement(): string {
     const headerStr = 'Date,Amount,Balance\n'
     let transactionsStr = ''
+    let currentBalance = 0
     this._ledger.transactions.forEach(transaction => {
-      transactionsStr += transaction.date + ',' + '+' + transaction.amount + ',' + transaction.amount + '\n'
+      currentBalance += transaction.amount
+      transactionsStr += transaction.date + ',' + '+' + transaction.amount + ',' + currentBalance + '\n'
     })
     return headerStr + transactionsStr
   }
